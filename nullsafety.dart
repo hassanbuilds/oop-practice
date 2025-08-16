095ddc6593efa5bb47a49b3dc8dd202dae97a4a8
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-16 13:50:35 -0700

    Start null safety practice file

--- nullsafety.dart
+++ nullsafety.dart
@@ -1,4 +1,8 @@
-void main() {
-  int? age;
-  print(age);
+//-----------------------------Basic Example------------------------------
+// Q1: Declare a nullable String? variable called 'name'.
+//     Don’t assign any value. Print it.
+
+void basicExample() {
+  String? name; // nullable String variable, not assigned any value
+  print(name); //  output: null
 }
