addb8df7646b6ea04572dcc7fcede7404bac599b
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-26 01:22:48 +0500

    Intermediate level practice complete

--- record.dart
+++ record.dart
@@ -2,14 +2,41 @@
 
 // Q1: Create a record (String, int) representing a book (title, pages).
 // Print both values.
+void main() {
+  var book = (Title: 'OPPS by practice by Hassan', Pages: 69);
+  print('Book:${book.Title}');
+  print('Pages:${book.Pages}');
+}
+
+// OutPut:Book: OOP by Practice by Hassan
+//  Pages: 69
 
 //------------------Intermediate Example ------------------------
 
 // Q2: Create a record (String name, int age, String city) for a user.
 // Use named fields. Print only city.
 
+void main2() {
+  var user = (name: 'Muhammad Hassan', city: 'Lahore');
+  print('City ${user.city}');
+}
+
+// Output;  City: Lahore
+
 // Q3: Write a function swap((int, int) pair) that returns a new record with swapped values.
 // Example: (3, 7) → (7, 3).
+
+(int, int) swap((int, int) pair) {
+  return (pair.$2, pair.$1); // $1 is first value, $2 is second
+}
+
+void main3() {
+  var numbers = (3, 7);
+  var swapped = swap(numbers);
+
+  print("Original: $numbers");
+  print("Swapped: $swapped");
+}
 
 //-------------------Advanced Example -------------------------------
 
