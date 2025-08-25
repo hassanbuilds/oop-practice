87163fd8322cd1e838747f78c0ebee25d088e1cf
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-25 05:22:02 +0500

    practice question for mixin added

---
+++ mixin.dart
@@ -0,0 +1,26 @@
+// ----------------------- Basic (Understand mixin basics) -----------------------------
+
+// Q1: Create a mixin Logger with a method log(String message).
+// - Create a class FileManager that uses Logger.
+// - In main(), create FileManager and call log().
+
+
+
+
+
+// -------------------- Intermediate (Use multiple mixins) -------------------------------
+
+// Q2: Create two mixins Flyable (fly()) and Swimmable (swim()).
+// - Create a class Duck that uses both Flyable and Swimmable.
+// - In main(), call both methods on Duck.
+
+
+
+
+
+//--------------------- Advanced  level(Mixin with constraints) ------------------------------
+
+// Q3: Create an abstract class Animal with method sound().
+// - Create a mixin Runner that requires Animal and adds run().
+// - Create a class Dog that extends Animal and uses Runner.
+// - In main(), create Dog and call sound() and run().
