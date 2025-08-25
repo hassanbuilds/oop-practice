d7c75f907b61c2e0edb865238739b413152f61c3
Muhammad Hassan <mhassan.codes@gmail.com>
2025-08-25 06:22:41 +0500

    enums practice started

---
+++ enums.dart
@@ -0,0 +1,48 @@
+//-------------------- Basic level -----------------------------------------
+// Q1: Create an enum Direction with values north, south, east, west.
+// - Write a function move() that prints "Moving <direction>".
+// - In main(), call move() with two different directions.
+
+
+
+
+// Q2: Create an enum Season with values spring, summer, autumn, winter.
+// - Write a function getSeasonActivity() that returns an activity for each season.
+// - Use switch-case inside the function.
+// - In main(), print activity for Season.summer.
+
+
+// ------------------- Intermediate Level --------------------------------
+
+
+// Q3: Create an enum OrderStatus with values pending, processing, shipped, delivered.
+// - Write a function checkStatus() that prints a different message for each status.
+// - In main(), simulate changing order status step by step.
+
+
+
+
+
+// Q4: Create an enum PaymentMethod with values cash, creditCard, debitCard, paypal.
+// - Write a function pay(amount, method) that prints "Paid <amount> using <method>".
+// - In main(), call pay() with two different methods.
+
+
+//------------------------- Advanced Level  ---------------------------------
+
+
+
+
+// Q5: Create an enum Role { admin, editor, viewer }.
+// - Create a class User with fields: name and role.
+// - Add a method permissions() that prints permissions based on role.
+// - In main(), create 3 users with different roles and show their permissions.
+
+
+
+
+
+
+// Q6: Create an enum TrafficLight { red, yellow, green } with an extension.
+// - Add a method action() inside the extension that returns "Stop", "Wait", or "Go".
+// - In main(), call action() for each traffic light value using a loop.
